79edca8a4856b7b2617f77d63f32bbd3b9ca7995
hamiz <reshma9298h@gmail.com>
2023-12-21 19:25:33 +0530

    commit

--- utils/config.js
+++ utils/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     MONGODB_URI: `mongodb+srv://reshma9298h:hamizmongo@hamizclust.dpaalqo.mongodb.net/Password-Reset`,
     PORT: process.env.PORT || 3001,
-    clientURL: "http://localhost:5173"
+    clientURL: "https://massivehaamid.github.io/day41fe-psd-reset/"
 };
